dd07a3cb5a1c0f59ff2123d326b3cca2aeadebdb
Qudratjon14 <samidaqudratova@gmail.com>
2025-01-16 17:49:15 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+
+setInterval(() => {
+    let currentTime = new Date();
+
+    hours.innerHTML = (currentTime.getHours()<10 ? "0":"")+currentTime.getHours();
+    minutes.innerHTML = (currentTime.getMinutes()<10 ? "0":"")+currentTime.getMinutes();
+    seconds.innerHTML = (currentTime.getSeconds()<10 ? "0":"")+currentTime.getSeconds();
+},1000);
+
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+
+start.addEventListener("click", () => {
+    console.log("Start");
+});
+
+stop.addEventListener("click", () => {
+    console.log("Stop");
+});
